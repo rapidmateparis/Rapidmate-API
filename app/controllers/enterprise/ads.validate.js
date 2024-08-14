2f606b17e410d91ee11c30c12e38a25cee6169a3
avaleenbrush <evaleenblush@awgarstone.com>
2024-08-14 21:54:45 +0530

    add manage ads and switch rating api in mongodb and admin dashboard by kalim

--- app/controllers/enterprise/ads.validate.js
+++ app/controllers/enterprise/ads.validate.js
@@ -1,13 +1,16 @@
-const { validationResult } = require('../../../middleware/utils')
+const { validationResult } = require('../../middleware/utils')
 const { check } = require('express-validator')
+
 /**
  * Validates create new item request
  */
 exports.createItem = [
-  check('order_number').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
-  check('consumer_ext').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
-  check('ratingValue').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
-  check('comment').exists().withMessage('MISSING'),
+    check('enterprise_ext_id').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
+    check('title').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
+    check('description').exists().withMessage('MISSING'),
+    check('url').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
+    check('icon').exists().withMessage('MISSING'),
+    check('photo').exists().withMessage('MISSING'),
   (req, res, next) => {
     validationResult(req, res, next)
   }
@@ -17,12 +20,11 @@ exports.createItem = [
  * Validates update item request
  */
 exports.updateItem = [
-    check('ratingValue').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
-    check('comment').exists().withMessage('MISSING'),
+    check('is_active').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
     check('id').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
-  (req, res, next) => {
-    validationResult(req, res, next)
-  }
+    (req, res, next) => {
+        validationResult(req, res, next)
+    }
 ]
 
 /**
