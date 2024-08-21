003673df73c624b005bbe775f81b2a32e72fa596
syszoo <support@syszoo.com>
2024-08-21 23:27:15 +0530

    Changes

---
+++ app/controllers/deliveryboy/addressbook/address.validate.js
@@ -0,0 +1,26 @@
+const { validationResult } = require('../../../middleware/utils')
+const { check } = require('express-validator')
+/**
+ * Validates create new item request
+ */
+exports.validateAddressRequest = [
+    check('delivery_boy_ext_id').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
+    check('first_name').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
+    check('phone').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
+    (req, res, next) => {
+    validationResult(req, res, next)
+  }
+]
+
+
+exports.validateExtId = [
+  check('id')
+    .exists()
+    .withMessage('MISSING')
+    .not()
+    .isEmpty()
+    .withMessage('IS_EMPTY'),
+  (req, res, next) => {
+    validationResult(req, res, next)
+  }
+]
