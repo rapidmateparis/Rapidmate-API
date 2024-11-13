e478abb7277365bab1ecdacdb6487d8fb8d82d0b
syszoo <support@syszoo.com>
2024-11-13 10:23:09 +0530

    Changes

--- app/controllers/common/Notifications/notification.js
+++ app/controllers/common/Notifications/notification.js
@@ -68,7 +68,7 @@ exports.getNotificationByExtId = async (req, res) => {
     console.log(page);
     const ext_id = req.params.ext_id;
     console.log(ext_id);
-    const data = await Notification.find({ receiverExtId: ext_id,is_del: false }).skip(perPage * page).limit(perPage);
+    const data = await Notification.find({ receiverExtId: ext_id,is_del: false }).sort({createdAt:-1}).skip(page * perPage).limit(parPage);
     let message = "Items retrieved successfully";
     if (data.length <= 0) {
       message = "No notification found.";
