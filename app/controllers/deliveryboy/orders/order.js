907e351929b808c4f791f20fd3897155863bca21
syszoo <support@syszoo.com>
2025-01-09 13:52:44 +0530

    Shift order Changes

--- app/controllers/deliveryboy/orders/order.js
+++ app/controllers/deliveryboy/orders/order.js
@@ -468,7 +468,7 @@ exports.getItemByDeliveryBoyExtId = async (req, res) => {
         responseData.map(async (order) => {
           console.log(order);
           const locations = await fetch("SELECT * FROM rmt_enterprise_order_line WHERE order_id = ?", [order.id]);
-          const slots = await fetch("SELECT * FROM rmt_enterprise_order_slot WHERE enterprise_order_id = ? and delivery_boy_Id = ?", [order.id, order.delivery_boy_id]);
+          const slots = await fetch("SELECT * FROM rmt_enterprise_order_slot WHERE enterprise_order_id = ? and delivery_boy_Id = ? and date(slot_date)=date(now())", [order.id, order.delivery_boy_id]);
           return {
             ...order,
             slots,
