4aa0eff52fa2ef27ff3a235768fba6c31982c5d0
syszoo <support@syszoo.com>
2025-02-23 15:22:59 +0530

    Changes

--- app/controllers/enterprise/billing/address.validate.js
+++ app/controllers/enterprise/billing/address.validate.js
@@ -6,7 +6,6 @@ const { check } = require('express-validator')
 exports.validateAddressRequest = [
     check('enterprise_ext_id').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
     check('first_name').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
-    check('phone').exists().withMessage('MISSING').not().isEmpty().withMessage('IS_EMPTY'),
     (req, res, next) => {
     validationResult(req, res, next)
   }
