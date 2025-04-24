f7619913227d867e60b32baada653a74c996ce85
syszoo <support@syszoo.com>
2025-04-24 23:50:22 +0530

    3mins to 15mins

--- app/controllers/deliveryboy/orders/order.js
+++ app/controllers/deliveryboy/orders/order.js
@@ -2416,7 +2416,7 @@ const getOrderByOrderNumber = async (order_number) => {
 
 const getScheduleUnallocateOrderList = async () => {
   try {
-    return await fetch("select order_number from rmt_order where service_type_id =1 and delivery_boy_id is null and is_del = 0 and order_status ='ORDER_PLACED' and payment_on is not null and schedule_order_allocate_retry_count<=3 and schedule_date_time is not null and TIMESTAMPDIFF(minute, now(), schedule_date_time)<=3 limit 5", []);
+    return await fetch("select order_number from rmt_order where service_type_id =1 and delivery_boy_id is null and is_del = 0 and order_status ='ORDER_PLACED' and payment_on is not null and schedule_order_allocate_retry_count<=3 and schedule_date_time is not null and TIMESTAMPDIFF(minute, now(), schedule_date_time)<=15 limit 5", []);
   } catch (error) {
     //console.log(error);
   }
