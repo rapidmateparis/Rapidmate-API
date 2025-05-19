0e75ace01c82304bcc413700235f664d27e90ef5
avaleenbrush <evaleenblush@awgarstone.com>
2025-05-19 16:56:45 +0530

    fix server issue

--- app/middleware/authenticate.js
+++ app/middleware/authenticate.js
@@ -30,20 +30,20 @@ const authenticateUser = (req, res, next) => {
     const extId = verified?.ext_id || `A${verified?.data?.userId}`;
     const role_type = utils.getRoleFromExtId(extId);
 
-    req.user = {
-      ext_id: extId,
-      role: role_type
-    };
-
     // Admin can impersonate based on path
     if (role_type === 'ADMIN') {
+      req.query.logged_ext_id = extId;
+      req.query.role_type = role_type;
       const pathParts = path.split('/');
       const maybeExtId = pathParts[pathParts.length - 1];
       if (/^[ACDE]\w{13}$/.test(maybeExtId)) {
-        req.user.ext_id = maybeExtId;
+        req.query.ext_id = maybeExtId;
       }
+    }else{
+      req.query.ext_id = extId;
+      req.query.role = role_type;
     }
-
+    
     next();
   } catch (err) {
      return res.status(401).json(utils.buildResponseMessageContent(HttpStatusCode.Unauthorized, "Unauthorized", 1001, "Restricted to access this service. Please contact your administrator"));
